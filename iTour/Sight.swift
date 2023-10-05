af324432a3c2b21504cba13a1573588b34952ba3
Ivan Terziev <ivanterziev38@gmail.com>
2023-10-05 22:50:51 +0300

    Added sights to destinations

---
+++ iTour/Sight.swift
@@ -0,0 +1,18 @@
+//
+//  Sight.swift
+//  iTour
+//
+//  Created by Ivan Terziev on 5.10.23.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Sight {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
